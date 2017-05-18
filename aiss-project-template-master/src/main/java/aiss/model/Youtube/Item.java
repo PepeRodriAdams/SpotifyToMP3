0d70821f405fab30f4b51f43c141da991d34cf4d
PepeRodriAdams <ponieb@hotmail.com>
2017-05-18 16:23:39 +0200

    Youtube pojo de verdad
    Pojo class de youtube search añadida

---
+++ aiss-project-template-master/src/main/java/aiss/model/Youtube/Item.java
@@ -0,0 +1,35 @@
+
+package aiss.model.Youtube;
+
+
+public class Item {
+
+    private String kind;
+    private String etag;
+    private Id id;
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getEtag() {
+        return etag;
+    }
+
+    public void setEtag(String etag) {
+        this.etag = etag;
+    }
+
+    public Id getId() {
+        return id;
+    }
+
+    public void setId(Id id) {
+        this.id = id;
+    }
+
+}
